95d382c1f1029ab5464e2e7c0ea6cf2c194c5095
slammayjammay <slammayjammay@gmail.com>
2017-07-13 15:48:52 -0700

    Scope test and and combo section

--- test/index.js
+++ test/index.js
@@ -2,66 +2,163 @@ const { assert, expect } = require('chai');
 const combiner = require('../');
 
 describe('array-element-combiner', () => {
-	it('exports a function', () => {
-		assert(typeof combiner === 'function');
+	describe('exporting', () => {
+		it('exports a function', () => {
+			assert(typeof combiner === 'function');
+		});
 	});
 
 
-	it('throws error when not given an array', () => {
-		expect(() => combiner()).to.throw()
-	});
-	it('throws error when not given callbacks', () => {
-		expect(() => {
-			combiner(['val1', 'val2']);
-		}).to.throw();
-	});
-
-	let options = {
-		compare(a, b) {
-			return a.type === b.type;
-		},
-		combine(a, b) {
-			return a.value + b.value;
-		},
-		cancel(value) {
-			return value === 0;
-		}
-	};
-
-	it('returns the input if length is 0 or 1', () => {
-		let input = [];
-		assert(combiner(input, options) === input);
-		input = [1];
-		assert(combiner(input, options) === input);
+	describe('input validation', () => {
+		it('throws error when not given an array', () => {
+			expect(() => combiner()).to.throw()
+		});
+		it('throws error when not given callbacks', () => {
+			expect(() => {
+				combiner(['val1', 'val2']);
+			}).to.throw();
+		});
 	});
 
-	it('combines array of two elements', () => {
-		const input = [
-			{ type: 'yes', value: 1 },
-			{ type: 'yes', value: 1 }
-		];
-		const expected = [2];
 
-		expect(combiner(input, options)).to.eql(expected);
-	});
+	describe('functionality', () => {
+		let options = {
+			compare(a, b) {
+				return a.type === b.type;
+			},
+			combine(a, b) {
+				// yuck make less fuzzy
 
-	it('does not combine elements that should be skipped', () => {
-		const input = [
-			{ type: 'yes', value: 1 },
-			{ type: 'no', value: 1 }
-		];
-		const expected = input;
+				const operator = a.type;
+				const value = eval(`${a.value} ${operator} ${b.value}`);
 
-		expect(combiner(input, options)).to.eql(expected);
-	});
+				return { type: b.newType || b.type, value };
+			},
+			cancel(value) {
+				return value.value === 0;
+			}
+		};
 
-	it('can cancel two elements', () => {
-		const input = [
-			{ type: 'yes', value: 1 },
-			{ type: 'yes', value: -1 }
-		];
-		const expected = [];
+		it('does not modify inputs', () => {
+			const input = [{ key: 'value' }];
+			assert(combiner(input, options) === input);
+		});
 
-		expect(combiner(input, options)).to.eql(expected);
+		it('returns the input if length is 0 or 1', () => {
+			let input = [];
+			assert(combiner(input, options) === input);
+			input = ['val1'];
+			assert(combiner(input, options) === input);
+		});
+
+		it('combines array of two elements', () => {
+			const input = [
+				{ type: '+', value: 1 },
+				{ type: '+', value: 1 }
+			];
+			const expected = [{ type: '+', value: 2 }];
+
+			expect(combiner(input, options)).to.eql(expected);
+		});
+
+		it('does not combine elements that should be skipped', () => {
+			const input = [
+				{ type: 'do not', value: 1 },
+				{ type: 'compare', value: 1 }
+			];
+			const expected = input;
+
+			expect(combiner(input, options)).to.eql(expected);
+		});
+
+		it('can cancel two elements', () => {
+			const input = [
+				{ type: '+', value: 1 },
+				{ type: '+', value: -1 }
+			];
+			const expected = [];
+
+			expect(combiner(input, options)).to.eql(expected);
+		});
+
+		it('compares correct elements after cancelling', () => {
+			const input = [
+				{ type: '*', value: 10 },
+				{ type: '+', value: 1 },
+				{ type: '+', value: -1 },
+				{ type: '*', value: 10 }
+			];
+			const expected = [{ type: '*', value: 100 }];
+
+			expect(combiner(input, options)).to.eql(expected);
+		});
+
+		it('compares correct elements after combining', () => {
+			const input = [
+				{ type: '*', value: 16 },
+				{ type: '+', value: 1, newType: '+' },
+				{ type: '+', value: 5, newType: '+' },
+				{ type: '+', value: 10, newType: '*' }
+			];
+			const expected = [
+				{ type: '*', value: 256 }
+			];
+
+			expect(combiner(input, options)).to.eql(expected);
+		});
+
+		describe('c-c-c-COMBO!', () => {
+			it('cancels b then cancels a -- [a, b, b, a]', () => {
+				const input = [
+					{ type: '*', value: 5 },
+					{ type: '+', value: 1 },
+					{ type: '+', value: -1, newType: '*' },
+					{ type: '*', value: 0 }
+				];
+				const expected = [];
+
+				expect(combiner(input, options)).to.eql(expected);
+			});
+
+			it('cancels b then combines a -- [a, b, b, a]', () => {
+				const input = [
+					{ type: '*', value: 5 },
+					{ type: '+', value: 1 },
+					{ type: '+', value: -1, newType: '*' },
+					{ type: '*', value: 5 }
+				];
+				const expected = [
+					{ type: '*', value: 25 }
+				];
+
+				expect(combiner(input, options)).to.eql(expected);
+			});
+
+			it('combines a with value of (combines b) -- [a, b, b]', () => {
+				const input = [
+					{ type: '*', value: 5 },
+					{ type: '+', value: 1 },
+					{ type: '+', value: 1, newType: '*' }
+				];
+				const expected = [
+					{ type: '*', value: 10 }
+				];
+
+				expect(combiner(input, options)).to.eql(expected);
+			});
+
+			it('combines b with value of (combines a) -- [a, a, b]', () => {
+				const input = [
+					{ type: '+', value: 1 },
+					{ type: '+', value: 1, newType: '*' },
+					{ type: '*', value: 5 }
+				];
+				const expected = [
+					{ type: '*', value: 10 }
+				];
+
+				expect(combiner(input, options)).to.eql(expected);
+			});
+		});
 	});
 });
